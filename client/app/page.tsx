f4615f2055eb56934591efdae56dac9c74a4369d
ahmedaliv <ahmed195131@feng.bu.edu.eg>
2024-04-15 11:58:58 +0200

    ui: enh

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -9,20 +9,21 @@ export default function Home() {
       {!videoChat && (
         <>
           <div className="container mx-auto text-center mt-8">
-            <h1 className="text-4xl font-bold mb-4">
+            <h1 className="text-2xl sm:text-xl font-bold mb-4">
               Welcome to Moegle - Where Chance Meets Connection!
             </h1>
-            <p className="text-lg text-gray-700">
+            <p className="text-base md:text-md text-gray-700">
               Looking for a random chat buddy? You're in the right place! Moegle
               brings people together from all walks of life for spontaneous
               conversations, meaningful connections, and endless possibilities.
             </p>
-            <p className="text-lg text-gray-700 mt-4">
+            <p className="text-base md:text-md text-gray-700 mt-4">
               Whether you're here to make new friends, have deep discussions, or
               simply enjoy some casual banter, Moegle is your go-to destination.
               Say goodbye to boredom and hello to serendipity!
             </p>
-          <Form setVideoChat={setVideoChat} />
+
+            <Form setVideoChat={setVideoChat} />
           </div>
         </>
       )}
